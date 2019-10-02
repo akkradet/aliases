ee6dd652e92e0b64bf22528c529409aded823aee
akkradet <akkradet7@gmail.com>
2019-10-02 15:08:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 # Quick install: `curl -s https://git.io/Jem52 | bash`
 
 cd ~
-wget -q https://raw.githubusercontent.com/akkradet/aliases/master/aliases.txt
+wget -q https://raw.githubusercontent.com/akkradet/aliases/master/aliases.sh
 sed -i '/^# START CUSTOM ALIASES/,/^# END CUSTOM ALIASES/d;' ~/.bashrc
-cat aliases.txt >> ~/.bashrc
-rm aliases.txt
+cat aliases.sh >> ~/.bashrc
+rm aliases.sh
 echo " ✅ Bash aliases successfully loaded; type \`exec bash\` to apply changes"
